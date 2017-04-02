97cc5e41e81a2f1a19096894f456d9f1ddbc2f85
mgraf1 <mgraf@knox.edu>
2017-04-02 13:59:25 -0500

    Added project

---
+++ desktop/src/net/mikegraf/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.mikegraf.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.mikegraf.SlipSliding;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SlipSliding(), config);
+	}
+}
